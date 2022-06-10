ede336ae4334bab444ae803d48d119f80d63443a
PotOfGreed <tobmoe33@gmail.com>
2022-06-10 15:31:57 +0200

    MüllProSekunde

--- Assets/Scripts/StatisticsHandler.cs
+++ Assets/Scripts/StatisticsHandler.cs
@@ -19,14 +19,16 @@ public class StatisticsHandler : MonoBehaviour
 
     public void ShowTime()
     {
+        float trashPerSecond = 14/60f;
+        float trashPerMinute = 14f;
         int minutes = Mathf.FloorToInt(timer.timer / 60F);
         int seconds = Mathf.FloorToInt(timer.timer - minutes * 60);
         if (minutes > 0)
         {
-            text.text = "WoW du kek hast in " + minutes.ToString() + " Minuten und " + seconds.ToString() + " Sekunden ," + score.score.ToString() + " Stück Müll gesammelt";
+            text.text = "WoW du kek hast in " + minutes.ToString() + " Minuten und " + seconds.ToString() + " Sekunden ," + score.score.ToString() + " Stück Müll gesammelt. Das ist ziemlich Schlecht, denn in dieser Zeit sind ca. " + ((trashPerSecond * seconds)+(trashPerMinute * minutes)).ToString("0.00") + " Tonnen mehr Müll im Meer gelandet";
 
 
         }
-        else { text.text = "WoW du kek hast in " + seconds.ToString() + " Sekunden ," + score.score.ToString() + " Stück Müll gesammelt"; }
+        else { text.text = "WoW du kek hast in " + seconds.ToString() + " Sekunden ," + score.score.ToString() + " Stück Müll gesammelt. Das ist ziemlich Schlecht, denn in dieser Zeit sind ca. "+((trashPerSecond*seconds).ToString("0.00")) +" Tonnen mehr Müll im Meer gelandet"; }
     }
 }
